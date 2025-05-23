c98e5d21b8a48bd47e4761027c674b56f9047a3a
BRUNO SANTOS <bhas795002@gmail.com>
2025-05-23 11:50:49 -0300

    adicionando os elementos dentro do js e do car

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,6 @@
-
+const html = document.querySelector('html');
 const div_carrosel = document.getElementById('carousel');
+const carousel_title = document.getElementById('carousel-title');
 //carousel
 const imagem_1 = document.createElement('img');
 imagem_1.src = "img/imagem_1.jpg"
@@ -8,37 +9,40 @@ imagem_2.src = "img/imagem_2.jpg"
 const imagem_3 = document.createElement('img');
 imagem_3.src = "img/imagem_3.jpg"
 
-// let carouselArr = [];
+let carouselArr = [];
 
+//Array storage class
 // carouselArr.push(new Carousel("imagem_1.jpg","Esta é a nova Ranger Ford 2022. Verifique novidades.","lancamento.html"));
 // carouselArr.push(new Carousel("imagem_2.jpg","Ford a nossa história","#"));
 // carouselArr.push(new Carousel("imagem_3.jpg","Nova Ford Bronco Sport 2022","lancamento.html"));
 // Carousel.Start(carouselArr);
-// //Array storage class
 
-
-// //class Carousel
-// class Carousel {
-
+//class Carousel
+class Carousel {
     
-      
-//     static Start(arr){
-//         if(arr){
+    constructor(img, titulo, ancora){
+        this.img = img;
+        this.titulo = titulo;
+        this.ancora = ancora;
+    }
 
-//             if(arr.length > 0){
-//                 Carousel._sequence = 0;
-//                 Carousel._size = arr.length;
-//                 Carousel.Next(); //start
-//                 Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-//             }
+
+    static Start(arr){
+        if(arr){
+
+            if(arr.length > 0){
+                Carousel._sequence = 0;
+                Carousel._size = arr.length;
+                Carousel.Next(); //start
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },2000);
+            }
             
-//         } else {
-//             throw "Method Start need a Array Variable.";
-//         }
-//     }
+        } else {
+            throw "Method Start need a Array Variable.";
+        }
+    }
 
-//     static Next(){
+    static Next(){
         
-//     }
-// };
-div_carrosel.appendChild(imagem_1);
+    }
+}
--- js/compare.js
+++ js/compare.js
@@ -1,10 +1,42 @@
+// const img1 = document.querySelector("img/XL Cabine.jpg");
+// const img2 = document.querySelector("img/xls 2.2 diesel.jpg");
+// const img3 = document.querySelector("img/storm.jpg");
+
+//for table
+const tabela = document.getElementById('compare');
+const comparacao_img0 = document.getElementById('compare_image_0');
+const comparacao_img1 = document.getElementById('compare_image_1');
+const comparacao_modelo0 = document.getElementById('compare_modelo_0');
+const comparacao_modelo1 = document.getElementById('compare_modelo_1');
+const comparacao_altura0 = document.getElementById('compare_alturacacamba_0');
+const comparacao_altura1 = document.getElementById('compare_alturacacamba_1');
+const comparacao_altura_cacamba0 = document.getElementById('compare_alturacacamba_0');
+const comparacao_altura_cacamba1 = document.getElementById('compare_alturacacamba_1');
+const comparacao_altura_do_solo0 = document.getElementById('compare_alturasolo_0');
+const comparacao_altura_do_solo1 = document.getElementById('compare_alturasolo_1');
+const comparacao_capacidade_carga0 = document.getElementById('compare_capacidadecarga_0');
+const comparacao_cacacidade_carga1 = document.getElementById('compare_capacidadecarga_1');
+const comparacao_motor0 = document.getElementById('compare_motor_0');
+const comparacao_motor1 = document.getElementById('compare_motor_1');
+const compare_potencia_0 = document.getElementById('compare_potencia_0');
+const compare_potencia_1 = document.getElementById('compare_potencia_1');
+const compare_volumecacamba_0 = document.getElementById('compare_volumecacamba_0');
+const compare_volumecacamba_1 = document.getElementById('compare_volumecacamba_1');
 
 //car
-let carArr = [];
-
 class Car {
     constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-       
+       this.nome = nome;
+       this.preco = preco;
+       this.alturaCacamba = alturaCacamba;
+       this.alturaVeiculo = alturaVeiculo;
+       this.alturaSolo = alturaSolo;
+       this.capacidadeCarga = capacidadeCarga;
+       this.motor = motor;
+       this.potencia = potencia;
+       this.volumeCacamba = volumeCacamba;
+       this.roda = roda;
+    //    this.image = image;
     }
 } 
 
@@ -34,7 +66,11 @@ function SetCarToCompare(el, carClass) {
 function ShowCompare() {
     if(carArr.length < 2) {
         alert("Precisa marcar 2 carros para apresentar a comparação");
-        return;
+        return 0;
+    }
+    else{
+        tabela.setAttribute('display', 'block');
+
     }
 
     UpdateCompareTable();
@@ -48,3 +84,11 @@ function HideCompare(){
 function UpdateCompareTable() {
     
 }
+
+let carArr = [];
+carArr.push(new Car("XL Cabine Simples 2.2 Diesel 4X4 MT 2022", 183.850, "0.511 m","1.816 m", "232 mm", "1.234 kg", "2.2 Turbo Diesel, 4 cilindros", 160,"1.800 litros (cabine simples)", "Aço 16 polegadas com pneus 255/70 R16" ));
+carArr.push(new Car("Ranger XLS 2.2 Diesel 4x4 AT 2022", 220.690, "0.511 m","1.816 m", "232 mm", "1.096 kg", "Motor: 2.2 Turbo Diesel, 4 cilindros", 160 ,"1.800 litros (cabine simples)", "Liga leve 17 polegadas com pneus All Terrain 265/65 R17" ));
+carArr.push(new Car("Ranger Storm 3.2 Diesel 4x4 AT 2022",  222.790, "0.511 m","1.849 m", "232 mm", "1.002 kg", "3.2 Turbo Diesel, 5 cilindros", 200,"1.800 litros (cabine simples)", " Liga leve 17 polegadas com pneus All Terrain 265/65 R17" ));
+
+
+console.log(carArr);
